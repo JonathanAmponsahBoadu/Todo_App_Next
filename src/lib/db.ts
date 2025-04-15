cb10301f552292c16a92804dc447eccb9428fcae
JonathanAmponsahBoadu <kkwesi956@gmail.com>
2025-04-15 22:54:46 +0000

    Changed connection String

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,10 @@
 import {Pool} from "pg";
 
 const pool = new Pool({
-    user : process.env.db_user,
-    password : process.env.db_password,
-    host : process.env.db_host,
-    port: Number(process.env.db_port),
-    database : process.env.db_name
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false, // Required for Supabase and most cloud Postgres
+    },
 }
 
 )
